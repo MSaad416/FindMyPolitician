c536b145ced7697c1f0ceb80b46949ddfbe422f2
Saad <msi.6ix@gmail.com>
2023-06-23 20:03:32 -0400

    updated to hide private key

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import axios from "axios";
+import dotenv from "dotenv";
 
 // __dirname due to changing type: module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+dotenv.config();
+
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 var latitude = 0;
 var longitude = 0;
@@ -16,9 +19,9 @@ var longitude = 0;
 // with react build folder
 // app.use(express.static());
 
-const convertGeoData = async (postalCode) => {
+const convertGeoData = async (postalCode, access_key) => {
   const params = {
-    access_key: "bad9524e22348d9587f8b6ff5918fd23",
+    access_key: access_key,
     query: `${postalCode}`,
     country: "CA",
   };
@@ -48,11 +51,7 @@ app.get("/", (req, res) => {
 app.get("/forward", (req, res) => {
   const { query } = req.query;
 
-  // console.log(access_key);
-  // console.log(query);
-  // console.log(country);
-
-  convertGeoData(query).then((response) => {
+  convertGeoData(query, process.env.ACCESS_KEY).then((response) => {
     if (response) {
       latitude = response.data.data[0].latitude;
       longitude = response.data.data[0].longitude;
@@ -63,14 +62,6 @@ app.get("/forward", (req, res) => {
     // res.json(`latitude : ${latitude}, longitude:  ${longitude}`);
     res.json([latitude, longitude]);
   });
-
-  // const latitude = data.data[0].latitude;
-  // const longitude = data.data[0].longitude;
-  // console.log("GEOCODING:");
-  // console.log(data);
-  // console.log(longitude);
-
-  // res.send(`Node Server run @ ${PORT}`);
 });
 
 app.listen(PORT, () => {
